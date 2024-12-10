1bac382f5b3495aadd848066cc6c09ce6e748fd7
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-10 22:06:46 +0700

    feature(chatbot): Add chatbot menu to navbar

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatPage(): React.ReactElement {
+  return (
+    <main className="w-full min-h-screen">
+      <h1>Hi! How can Lexicon help you?</h1>
+    </main>
+  );
+}
