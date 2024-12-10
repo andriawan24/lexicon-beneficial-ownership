a59688f667bf98bdff774e1c4007bb364df629a3
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-10 21:59:57 +0700

    feature(favicon): Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,26 @@ export const metadata: Metadata = {
     description: siteConfig.description,
     siteName: siteConfig.name,
   },
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+  ],
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
