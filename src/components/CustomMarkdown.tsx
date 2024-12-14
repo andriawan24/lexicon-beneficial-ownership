1a63c90b5ea79716bb7331a8b1619a8518f54ed4
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-14 14:43:36 +0700

    feature(chatbot): Add CustomMarkdown component

--- src/components/CustomMarkdown.tsx
+++ src/components/CustomMarkdown.tsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 
 interface Props {
   source: string;
@@ -15,9 +15,13 @@ export default function CustomMarkdown(props: Props): ReactElement {
             {elements.children}
           </h1>
         ),
-        ul: (elements) => <ul {...elements}>{elements.children}</ul>,
+        ul: (elements) => (
+          <ul className="list-disc" {...elements}>
+            {elements.children}
+          </ul>
+        ),
         li: (elements) => (
-          <li {...elements} className="list-disc ml-4">
+          <li {...elements} className="ml-4">
             {elements.children}
           </li>
         ),
