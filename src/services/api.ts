34c677e3eb404e6b09b98bf57cedd7318646d2b1
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-15 21:10:29 +0700

    feature(chatbot): Finalizing chatbot and optimizing for mobile devices

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://beneficialowner.lexicon.id/api'
+export const BASE_URL = "https://beneficialowner.lexicon.id/api";
+export const BASE_URL_CHATBOT = "https://7627-103-121-108-197.ngrok-free.app";
